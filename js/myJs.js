df0f90014e3fb7fbc9769ddefbbb2f8db52e5684
DenisAleshkov <aleshkov592@gmail.com>
2020-01-02 18:37:50 +0300

    v2

--- js/myJs.js
+++ js/myJs.js
@@ -1,13 +1,23 @@
 let dropdownButton = document.querySelectorAll('.dropdown__title_button');
 let dropdownBlock = document.querySelectorAll('.dropdown__item_content');
+let modalForm = document.getElementById('modal');
+let modalButton = document.getElementById('modal__button');
+let exitButton = document.getElementById('exit');
 
 
 for (let i = 0; i < dropdownButton.length; i++) {
-    
-    dropdownButton[i].addEventListener('click',function(){
+
+    dropdownButton[i].addEventListener('click', function () {
         if (dropdownButton[i].dataset.number === dropdownBlock[i].dataset.number) {
             dropdownBlock[i].classList.toggle('is_open');
             dropdownButton[i].classList.toggle('transform_button');
         }
     });
+};
+modalButton.onclick=function(){
+    modalForm.style.display='flex';
 }
+exitButton.onclick=function(){
+    modalForm.style.display='none';
+}
+
