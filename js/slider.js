e07f30eb2304c4ba9047b980743b95e4d812d829
DenisAleshkov <aleshkov592@gmail.com>
2020-01-16 09:34:03 +0300

    add slider anim

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+let sliderItem = document.querySelectorAll('.slider__item');
+let buttonUp = document.getElementById('button-up');
+let buttonDown = document.getElementById('button-down');
+
+let current = 0;
+
+
+function slider(){
+    
+	for(let i=0; i < sliderItem.length; i++){
+ 		sliderItem[i].classList.add('opacity0');
+	}
+
+    sliderItem[current].classList.remove('opacity0');
+
+}
+
+slider();
+
+buttonUp.onclick=function(){
+	current===sliderItem.length-1 ? current=0: current++
+	slider();
+}
+
+buttonDown.onclick=function(){
+	current--;
+	current===-1 ? current=sliderItem.length-1
+	slider();
+}
